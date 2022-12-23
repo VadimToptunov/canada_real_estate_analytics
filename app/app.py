be8135673041e860bbf2f98aeca68a55818e79ae
vadimtoptunov <vtoptunov88@gmail.com>
2022-12-23 17:41:03 +0200

    Try to fix getting data from the DB. ToDo: Fix it

--- app/app.py
+++ app/app.py
@@ -1,17 +1,25 @@
 import asyncio
 import os
 
-from server import db
+from flask_sqlalchemy import SQLAlchemy
+
+from app.server import models
 from apscheduler.schedulers.background import BackgroundScheduler
 from data_gatherer import gather_rent_data
 from flask import Flask, jsonify, render_template, send_from_directory
 from flask_cors import CORS, cross_origin
 
-template_dir = os.path.abspath('./client/templates/')
-static_dir = os.path.abspath('./client/static/')
+template_dir = os.path.abspath('./app/client/templates/')
+static_dir = os.path.abspath('./app/client/static/')
+db_path = "./gathering_scripts/rent-data-canada/database/apptdata.db"
 appFlask = Flask(__name__, template_folder=template_dir, static_folder=static_dir)
 cors = CORS(appFlask)
 appFlask.config['CORS_HEADERS'] = 'Content-Type'
+appFlask.config['SQLALCHEMY_DATABASE_URI'] = \
+    'sqlite:///' + os.path.join(db_path)
+appFlask.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(appFlask)
 
 
 @appFlask.route('/', methods=['GET'])
@@ -23,11 +31,26 @@ def index():
 @appFlask.route('/data', methods=['GET'])
 @cross_origin()
 def get_data_from_db():
-    flats = [b.serialize() for b in db.view()]
+    flats = models.Flats
+    flats_data = db.session.query(flats)
+    db.session.commit()
+    for flat in flats_data:
+        return jsonify({
+            'status': 200,
+            'homeid': flat.homeid,
+            'latlong': flat.latlong,
+            'latitude': flat.latitude,
+            # self.longitude = self.db.Column(self.db.String)
+            # self.postal_code = self.db.Column(self.db.String)
+            # self.fsa = self.db.Column(self.db.String)
+            # self.rent_price = self.db.Column(self.db.Integer)
+        })
+
+    # flats = [b.serialize() for b in db.view()]
     return jsonify({
-        'res': flats,
+        'res': flatsData,
         'status': '200',
-        'no_of_prices': len(flats)
+        'no_of_prices': len(flatsData)
     })
 
 
--- app/server/db.py
+++ app/server/db.py
@@ -1,32 +1,65 @@
 import sqlite3
+
+from flask_sqlalchemy.model import Model
+from sqlalchemy.testing import db
+
 from . import models
 
-TABLENAME = "rent_prices"
-DB_PATH = "../gathering_scripts/rent-data-canada/database/apptdata.db"
+from flask_sqlalchemy import SQLAlchemy
 
 
-def connect():
-    conn = sqlite3.connect(DB_PATH)
-    cur = conn.cursor()
-    cur.execute(f"""CREATE TABLE IF NOT EXISTS {TABLENAME}(
-        latitude TEXT,
-        longitude TEXT,
-        postal_code TEXT,
-        fsa TEXT,
-        rent_price INTEGER)""")
-    conn.commit()
-    conn.close()
+# TABLENAME = "rent_prices"
 
 
-def view():
-    conn = sqlite3.connect(DB_PATH)
-    cur = conn.cursor()
-    cur.execute(f"""SELECT DISTINCT latitude, longitude, fsa, round(avg(rent_price), 2) 
-AS average_price FROM {TABLENAME} GROUP BY fsa;""")
-    rows = cur.fetchall()
-    flats = []
-    for i in rows:
-        flat = models.Flats(i[0], i[1], i[2], i[3])
-        flats.append(flat)
-    conn.close()
-    return flats
+class Flats():
+    query = None
+    db = None
+    homeid = ""
+    latlong = ""
+    latitude = ""
+    longitude = ""
+    postal_code = ""
+    fsa = ""
+    rent_price = 0
+
+    def __init__(self, flask_sqlalchemy):
+        self.db = SQLAlchemy(flask_sqlalchemy)
+        self.setDB()
+
+    def setDB(self):
+        self.homeid = self.db.Column(self.db.String, primary_key=True)
+        self.latlong = self.db.Column(self.db.String, unique=True)
+        self.latitude = self.db.Column(self.db.String)
+        self.longitude = self.db.Column(self.db.String)
+        self.postal_code = self.db.Column(self.db.String)
+        self.fsa = self.db.Column(self.db.String)
+        self.rent_price = self.db.Column(self.db.Integer)
+
+    def __repr__(self):
+        return f'<Flat {self.homeid} {self.postal_code}>'
+
+# def connect():
+#     conn = sqlite3.connect(DB_PATH)
+#     cur = conn.cursor()
+#     cur.execute(f"""CREATE TABLE IF NOT EXISTS {TABLENAME}(
+#         latitude TEXT,
+#         longitude TEXT,
+#         postal_code TEXT,
+#         fsa TEXT,
+#         rent_price INTEGER)""")
+#     conn.commit()
+#     conn.close()
+
+
+# def view():
+#     conn = sqlite3.connect(DB_PATH)
+#     cur = conn.cursor()
+#     cur.execute(
+#         f"""SELECT latitude, longitude, fsa, round(avg(rent_price), 2) AS average_price FROM {TABLENAME} group by fsa;""")
+#     rows = cur.fetchall()
+#     flats = []
+#     for i in rows:
+#         flat = models.Flats(i[0], i[1], i[2], i[3])
+#         flats.append(flat)
+#     conn.close()
+#     return flats
